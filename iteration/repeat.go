8470ad2d80db3aea3cc1f1436dcb2a34e490a4ca
MacNaughty <evan.cavelaars@outlook.com>
2021-03-19 14:04:36 -0700

    integer and iteration modules completed. About to start on practice exercises for iterations

---
+++ iteration/repeat.go
@@ -0,0 +1,12 @@
+package iteration
+
+import ()
+
+const repeatCount = 5
+
+func Repeat(character string) (result string) {
+	for i := 0; i < repeatCount; i++ {
+		result += character
+	}
+	return
+}
