15209b4f304f18f1cb4ae70f9cb7d127ac382742
MacNaughty <evan.cavelaars@outlook.com>
2021-03-21 18:50:26 -0700

    About to test the order of sleep calls (currently just verifying the number of times Sleeper's Sleep() method is called)

---
+++ mocking/countdown.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, s Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		s.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	s.Sleep()
+	fmt.Fprintf(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 4 {
+		t.Errorf("wrong number of sleeper calls, want 4 got %d", spySleeper.Calls)
+	}
+}
