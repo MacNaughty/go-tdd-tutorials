0da99cd5eb5212f5d1c06fe0987653ec95cade00
MacNaughty <evan.cavelaars@outlook.com>
2021-03-24 00:57:06 -0700

    Refactored and cleaned up first select method test

---
+++ select_module/websiteracer_test.go
@@ -0,0 +1,38 @@
+package select_module
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	//slowURL := "http://www.facebook.com"
+	slowURL := slowServer.URL
+	//fastURL := "http://www.quii.co.uk"
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
