b302e9d39275c8f09433fd607a8a1164d82a7e05
MacNaughty <evan.cavelaars@outlook.com>
2021-04-14 02:44:13 -0700

    Finished covert to arabic method. About to refactor

--- properties/roman_numerals.go
+++ properties/roman_numerals.go
@@ -1,30 +1,78 @@
 package properties
 
-import "strings"
+import (
+	"strings"
+)
+
+type RomanNumeral struct {
+	Arabic int
+	Roman  string
+}
+
+var allRomanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
 
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Arabic {
+			result.WriteString(numeral.Roman)
+			arabic -= numeral.Arabic
 		}
 	}
 
 	return result.String()
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if couldBeSubtractive(i, symbol, roman) {
+			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
+				total += value
+				i++ // move past this character too for the next loop
+			} else {
+				total += allRomanNumerals.ValueOf(symbol)
+			}
+		} else {
+			total += allRomanNumerals.ValueOf(symbol)
+		}
+	}
+	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+	return index+1 < len(roman) && isSubtractiveSymbol
+}
+
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Roman == symbol {
+			return s.Arabic
+		}
+	}
+
+	return 0
 }
